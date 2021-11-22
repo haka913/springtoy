57101a24ed39cbfb28575d890017267e6a743674
paul <ace95913@gmail.com>
2021-11-22 23:14:51 +0900

    ch4 mustache query(select *)

--- src/main/java/com/haka/boot/springboot/service/posts/PostsService.java
+++ src/main/java/com/haka/boot/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.haka.boot.springboot.service.posts;
 
 import com.haka.boot.springboot.domain.posts.Posts;
 import com.haka.boot.springboot.domain.posts.PostsRepository;
+import com.haka.boot.springboot.web.dto.PostsListResponseDto;
 import com.haka.boot.springboot.web.dto.PostsResponseDto;
 import com.haka.boot.springboot.web.dto.PostsSaveRequestDto;
 import com.haka.boot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -26,8 +30,14 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
