ee38b88b516cfa580a992b7ac8443d5bb97112a7
paul <ace95913@gmail.com>
2021-11-22 21:09:04 +0900

    ch3 Posts API controller, service, Dto, test

---
+++ src/main/java/com/haka/boot/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.haka.boot.springboot.service.posts;
+
+import com.haka.boot.springboot.domain.posts.PostsRepository;
+import com.haka.boot.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
