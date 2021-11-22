6e051d79a22997c80fcd192107628a42316ee733
paul <ace95913@gmail.com>
2021-11-22 21:29:48 +0900

    ch3 Posts API controller, service, Dto, test (update,findById)

--- src/main/java/com/haka/boot/springboot/service/posts/PostsService.java
+++ src/main/java/com/haka/boot/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.haka.boot.springboot.service.posts;
 
+import com.haka.boot.springboot.domain.posts.Posts;
 import com.haka.boot.springboot.domain.posts.PostsRepository;
+import com.haka.boot.springboot.web.dto.PostsResponseDto;
 import com.haka.boot.springboot.web.dto.PostsSaveRequestDto;
+import com.haka.boot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +16,18 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
