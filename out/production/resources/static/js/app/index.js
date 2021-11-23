08e01c24f760fa2d49c3c3088fbfc4fdc8eb464b
paul <ace95913@gmail.com>
2021-11-23 16:30:25 +0900

    ch5 oauth(google) refactoring with annotaion (GUEST 에러 정상적으로 작동)

--- out/production/resources/static/js/app/index.js
+++ out/production/resources/static/js/app/index.js
@@ -11,7 +11,7 @@ var main = {
             _this.delete();
         });
     },
-    save: function () {
+    save : function () {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -22,9 +22,9 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
---
+++ src/main/java/com/haka/boot/springboot/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.haka.boot.springboot.config;
+
+import com.haka.boot.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
